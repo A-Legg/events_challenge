3ef4304bdc1705d335ba846c4f8690ce8d34e3e6
Adam Legg <adam.legg@gmail.com>
2015-05-15 12:46:02 -0600

    updated model methods to reflect proper track, stuck on being able to dynamically add events from multiple tracks based on time

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,23 +5,23 @@ class Event < ActiveRecord::Base
 		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
 	end
 	def self.track_two
-		 track_one = Event.where('track LIKE ?', '%Track 2%') 
+		 track_two = Event.where('track LIKE ?', '%Track 2%') 
 		 group_events = Event.where.not('track LIKE ?', '%Track%')
-		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+		 (track_two << group_events).flatten!.sort_by { |k| k["start"] }
 	end
 	def self.track_three
-		 track_one = Event.where('track LIKE ?', '%Track 3%') 
+		 track_three = Event.where('track LIKE ?', '%Track 3%') 
 		 group_events = Event.where.not('track LIKE ?', '%Track%')
-		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+		 (track_three << group_events).flatten!.sort_by { |k| k["start"] }
 	end
 	def self.track_four
-		 track_one = Event.where('track LIKE ?', '%Track 4%') 
+		 track_four = Event.where('track LIKE ?', '%Track 4%') 
 		 group_events = Event.where.not('track LIKE ?', '%Track%')
-		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+		 (track_four << group_events).flatten!.sort_by { |k| k["start"] }
 	end
 	def self.track_five
-		 track_one = Event.where('track LIKE ?', '%Track 5%') 
+		 track_five = Event.where('track LIKE ?', '%Track 5%') 
 		 group_events = Event.where.not('track LIKE ?', '%Track%')
-		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+		 (track_five << group_events).flatten!.sort_by { |k| k["start"] }
 	end
 end
