5ee85009a55f4dd367de83912d3ceabe5da7f62a
Adam Legg <adam.legg@gmail.com>
2015-05-15 10:48:19 -0600

    fixed view format and added track methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,27 @@
 class Event < ActiveRecord::Base 
+	def self.track_one
+		 track_one = Event.where('track LIKE ?', '%Track 1%') 
+		 group_events = Event.where.not('track LIKE ?', '%Track%')
+		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+	end
+	def self.track_two
+		 track_one = Event.where('track LIKE ?', '%Track 2%') 
+		 group_events = Event.where.not('track LIKE ?', '%Track%')
+		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+	end
+	def self.track_three
+		 track_one = Event.where('track LIKE ?', '%Track 3%') 
+		 group_events = Event.where.not('track LIKE ?', '%Track%')
+		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+	end
+	def self.track_four
+		 track_one = Event.where('track LIKE ?', '%Track 4%') 
+		 group_events = Event.where.not('track LIKE ?', '%Track%')
+		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+	end
+	def self.track_five
+		 track_one = Event.where('track LIKE ?', '%Track 5%') 
+		 group_events = Event.where.not('track LIKE ?', '%Track%')
+		 (track_one << group_events).flatten!.sort_by { |k| k["start"] }
+	end
 end
