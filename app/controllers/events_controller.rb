454659953084a1800df4e7ac685bb83a197124ee
Adam Legg <adam.legg@gmail.com>
2015-05-15 10:07:57 -0600

    events sortable by day

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController 
 	def index 
+		@day_one_events = Event.where('start < ?', Time.zone.parse('2015-09-12')).order(start: :asc)
+		@day_two_events = Event.where('start > ?', Time.zone.parse('2015-09-12')).order(start: :asc)
 	end
 end
